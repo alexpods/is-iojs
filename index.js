b563c4ad6caaa2a1c25d9285c3ca5608369f13cf
alexpods <aleksey.podskrebyshev@gmail.com>
2015-02-01 15:36:41 +0300

    Remove unnecessary  funciton name

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function() {
     return _isIo;
 };
 
-module.exports.safe = function self() {
+module.exports.safe = function() {
     var isIo = _isIo;
 
      !~['v1.0.0','v1.0.1'].indexOf(version) && require('child_process').exec('node -h', function(err, help) {
