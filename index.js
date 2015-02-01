aae32dc625ddd2ee6612f334adfd4566325d92c4
alexpods <aleksey.podskrebyshev@gmail.com>
2015-02-01 15:20:47 +0300

    Simple and safe logic for iojs detection

---
+++ index.js
@@ -0,0 +1,19 @@
+var version = process.version;
+var _isIo = parseInt(version.match(/^v(\d)/)[1]) >= 1;
+
+module.exports = function() {
+    return _isIo;
+};
+
+module.exports.safe = function self() {
+    var isIo = _isIo;
+
+     !~['v1.0.0','v1.0.1'].indexOf(version) && require('child_process').exec('node -h', function(err, help) {
+        if (err) ioIo = false;
+        isIo = /iojs/m.test(help);
+    });
+
+    return function() {
+        return isIo;
+    };
+};
